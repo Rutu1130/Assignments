b5e40439bfe690e4893b26ebca202bf23784b311
Rutu1130 <154366245+Rutu1130@users.noreply.github.com>
2024-01-26 20:32:06 +0530

    Add files via upload

---
+++ Assignment1/Assignment1_3.c
@@ -0,0 +1,21 @@
+//Program to print 5 to 1 numbers  on screen 
+
+#include<stdio.h>
+
+void Display()
+{
+    //int i = 0;
+    int i=5;
+
+    for (i =5 ;i>=1;i--)
+    {
+        printf("%d",i);
+        //i++;
+    }
+}
+
+int main()
+{
+    Display();
+    return 0;
+}
---
+++ Assignment1/Assignment1_5.c
@@ -0,0 +1,20 @@
+// Aceept one number from user and print that number of * on screen..
+
+#include<stdio.h>
+
+void Accept(int iNo)
+{
+    int iCnt = 0;
+    for(iCnt= 0; iCnt <= iNo;iCnt++)
+    {
+        printf("*");
+    }
+}
+int main()
+{
+    int iValue = 0;
+    iValue = 5;
+
+    Accept(iValue);
+    return 0;
+}

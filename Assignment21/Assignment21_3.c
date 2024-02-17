0548060a3bf460c6a834bf11677fb98c48011f35
Rutu1130 <154366245+Rutu1130@users.noreply.github.com>
2024-02-17 12:13:01 +0530

    Add files via upload

---
+++ Assignment21/Assignment21_3.c
@@ -0,0 +1,52 @@
+/* Accept nuber of rows and number of columns from user and display below pattern.
+iRow= 4
+iCol = 4
+
+   $    *   *   *   *
+   *    $   *   *   *
+   *    *   $   *   *
+   *    *   *   $   * 
+   *    *   *   *   $
+*/
+
+
+#include<stdio.h>
+
+void Pattern(int iRow, int iCol)
+{
+    int i = 0, j = 0 ;
+
+    for(i = 1; i<=iRow; i++)
+    {
+        for(j = 1; j <= iCol ; j++)
+        {
+            if((j == i))        
+            {
+                printf("$\t",j);
+            }
+        
+            else
+            {
+                printf("*\t");
+
+            }
+            
+        }
+        printf("\n\n");
+    }
+
+
+}
+
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    printf("Enter number of rows and columns : \n");
+    scanf("%d %d", &iValue1, &iValue2);
+
+    Pattern(iValue1, iValue2);
+    return 0 ;
+
+}

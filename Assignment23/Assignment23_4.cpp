507625aeabf1b774d12932c0ee8115b81bd7a4f9
Rutu1130 <154366245+Rutu1130@users.noreply.github.com>
2024-02-19 09:45:21 +0530

    Add files via upload

---
+++ Assignment23/Assignment23_4.cpp
@@ -0,0 +1,84 @@
+/* Accept number of rows and number of columns from user and display below pattern
+
+Input :  iRows = 5     iCol = 5
+
+output :   
+    *   *   *   *   *
+    *   #    #  *   *
+    *   #   *   $   *
+    *   *  $    $   *
+    *   *   *   *   *
+           
+*/
+
+#include<iostream>
+using namespace std;
+
+class Pattern
+{
+    private:
+        int iRow;
+        int iCol;
+
+    public:
+        Pattern(int X, int Y)
+        {
+            iRow = X;
+            iCol = Y;
+        }
+
+        void Display()
+        {
+            int i = 0, j = 0;
+
+            for (i = iRow; i >= 1 ; i--)
+            {
+                for(j = 1; j <= iCol; j++)
+                {
+                    if (j == i)
+                    {
+                        cout << "*\t";
+                    }
+                    else if((i == iRow) || (j == 1 ))
+                    {
+                        cout << "*\t";
+                    }
+                    else if((j == iCol) || (i == 1 ))
+                    {
+                        cout << "*\t";
+                    }
+                    else if(j < i)
+                    {
+                        cout<<"#\t";
+                    }
+                    else if(j > i)
+                    {
+                        cout<<"$\t";
+                    }
+                }
+                cout << endl; 
+            }
+        }
+};
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    cout << "Enter number of rows : " << endl;
+    cin >> iValue1;
+
+    cout << "Enter number of columns : " << endl;
+    cin >> iValue2;
+
+    Pattern *pobj = new Pattern(iValue1, iValue2);
+
+    pobj->Display();
+    
+    delete pobj;
+    
+    return 0;
+}
+
+
+
